2c1f3ef1fc6d3348133ef452843a61f7fb2a7af0
alejandro <alejandroweil13@gmail.com>
2022-12-10 04:14:02 -0500

    response from api using function with desired results

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,21 +1,25 @@
 import React, {useState, useEffect} from "react";
-
+import axios from "axios";
 
 // Root component which holds api logic
 
 const Root = () => { 
 
+  // state variables
   const [searchValue, setSearchValue] = useState("")
   const [searchResults, setSearchResults] = useState(null)  
 
-
+  // updates state change for search input
   const handleChange = e => {
     setSearchValue(e.target.value)
   }
 
-  const fetchWiki = () => {
+  // fetching wikipedia data function
+  const fetchWiki = (search) => {
     const wikiEndpoint = 'https://simple.wikipedia.org/w/api.php'
     const wikiParams = '?action=query'
+    + "&list=search"
+    + `&srsearch=${search}`
     + "&exsentences=2" // request first 2 sentences from wiki page
     + "&explaintext=1" // requests API to provide content in plain text
     + "&format=json" // requests data in JSON format
@@ -34,7 +38,8 @@ const Root = () => {
       return res.data
     }
     return getWikiResponse(wikiLink, wikiConfig).then(res => {
-
+      setSearchResults(res)
+      console.log(searchResults)
     }).catch(err => console.log(err))
   }
 
@@ -43,7 +48,7 @@ const Root = () => {
       <div className="input-text-random-container">
         <span>Please click on the icon</span>
         <input className="search-input" type="text" onChange={handleChange} value={searchValue}/>
-        <button>Search</button>
+        <button onClick={() => fetchWiki(searchValue)}>Search</button>
       </div>
     </div>
     )
