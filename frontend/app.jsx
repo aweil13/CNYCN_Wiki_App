ee7c5d45903100855d5d4cc4d9875ae1992cbe58
alejandro <alejandroweil13@gmail.com>
2022-12-10 03:48:10 -0500

    built skeleton and initial API call

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -4,5 +4,5 @@ import Root from './components/root'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDom.render(Root, root);
+  ReactDom.render(<Root className="root-container"/>, root);
 });
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,11 +1,52 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-export default  Root = () => (
-  <div>
-    <header className="page-header">
-      <h1>
-        Wiki App
-      </h1>
-    </header>
-  </div>
-)
+
+// Root component which holds api logic
+
+const Root = () => { 
+
+  const [searchValue, setSearchValue] = useState("")
+  const [searchResults, setSearchResults] = useState(null)  
+
+
+  const handleChange = e => {
+    setSearchValue(e.target.value)
+  }
+
+  const fetchWiki = () => {
+    const wikiEndpoint = 'https://simple.wikipedia.org/w/api.php'
+    const wikiParams = '?action=query'
+    + "&exsentences=2" // request first 2 sentences from wiki page
+    + "&explaintext=1" // requests API to provide content in plain text
+    + "&format=json" // requests data in JSON format
+    + "&formatversion=2" // JSON easier to navigate
+    + "&origin=*" // ommiting causes error 
+
+    const wikiLink = wikiEndpoint + wikiParams
+    console.log(wikiLink)
+
+    const wikiConfig = {
+      timeout: 3000
+    }
+
+    async function getWikiResponse(url, config){
+      const res = await axios.get(url,config)
+      return res.data
+    }
+    return getWikiResponse(wikiLink, wikiConfig).then(res => {
+
+    }).catch(err => console.log(err))
+  }
+
+  return (  
+    <div className="main-page-container">
+      <div className="input-text-random-container">
+        <span>Please click on the icon</span>
+        <input className="search-input" type="text" onChange={handleChange} value={searchValue}/>
+        <button>Search</button>
+      </div>
+    </div>
+    )
+}
+
+export default Root;
